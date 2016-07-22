b6bc62d7891cf19fe6cfa99ef4f6d7662681d02d
LaubachLab <laubachcsn@gmail.com>
2016-07-22 11:56:19 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nexpy',
+      version='0.1',
+      description='',
+      url='https://github.com/LaubachLab/nexpy',
+      author='Kyra Swanson',
+      author_email='laubachcsn@gmail.com',
+      license='MIT',
+      packages=['nexpy'],
+      zip_safe=False)
